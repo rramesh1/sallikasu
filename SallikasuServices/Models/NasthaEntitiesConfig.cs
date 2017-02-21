bff6995cca8053d8f048e2bdc0248cf97bc2f217
Ramesh <2ramesh@gmail.com>
2017-02-20 18:50:03 -0700

    Added the project for rest services.

---
+++ SallikasuServices/Models/NasthaEntitiesConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SallikasuServices
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Infrastructure;
+
+    // need this for the runtime. the auto generated class gets overwritten every time.
+    [DbConfigurationType(typeof(MySql.Data.Entity.MySqlEFConfiguration))]
+    public partial class nasthaEntities : DbContext
+    {
+    }
+}
